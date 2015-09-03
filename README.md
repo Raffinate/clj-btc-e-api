31c7e79c32f7f696211592f8c16636851b46d134
Aleksey <moxobukob@gmail.com>
2015-09-03 20:58:48 +0600

    Added website and clojars info

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ A Clojure library designed to connect and trade on btc-e.com.
 
 ## Usage
 
+[![Clojars Project](http://clojars.org/clj-btc-e-api/latest-version.svg)](http://clojars.org/clj-btc-e-api)
+
 Use (new-api ...) to create your api settings.
 Use (get-<public-method-name> trade-pair) to get public info and
 (trade api method params) to apply trade api.
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-btc-e-api "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure library for btc-e.com api"
+  :url "https://github.com/Raffinate/clj-btc-e-api"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
