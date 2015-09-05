fc7916bd03559a4268a096ebd85b75634fbf2715
Aleksey <moxobukob@gmail.com>
2015-09-05 16:06:24 +0600

    Changed docstrings and rename new-api to new-stock function.

--- src/clj_btc_e_api/core.clj
+++ src/clj_btc_e_api/core.clj
@@ -9,39 +9,48 @@
 
 (defn- new-settings []
   (->Settings "https://btc-e.com"
-                   "api/2"
-                   "tapi"))
+              "api/2"
+              "tapi"))
 
 (defrecord Account [key secret])
 
 (defn- new-account [key secret]
   (->Account key secret))
 
-(defrecord Api [account settings nonce-fn])
+(defrecord Stock [account settings nonce-fn])
 
 (defn default-nonce
-  "shft: 0 - millis, 3 - seconds"
+  "Returns function that generates nonce.
+  This nonce generator use time in millis to generate nonce.
+  shft is a parameter that selects the interval of new nonce.
+  shft = 0 - each millisecond, 3 - each second e.t.c."
   [shft]
   #(mod (bit-shift-right (System/currentTimeMillis) shft) 4294967295))
 
 (defn atom-nonce
-  "val - initial value of nonce"
+  "Returns function that generates nonce.
+  Each call will generate incremental nonce.
+  val - initial value of nonce."
   [val]
   (let [n (atom val)]
     (fn []
       (swap! n inc))))
 
-(defn new-api
+(defn new-stock
+  "key - your key from btc-e.com
+  secret - your secret from btc-e.com
+  nonce-fn - 0-arity function that returns new incremental values each call.
+  You can use default-nonce and atom-nonce to create nonce-fn."
   ([]
-   (new-api nil nil nil))
+   (new-stock nil nil nil))
   ([key secret]
-   (new-api key secret (default-nonce 2)))
+   (new-stock key secret (default-nonce 2)))
   ([key secret nonce-fn]
-   (->Api (new-account key secret)
-          (new-settings)
-          nonce-fn)))
+   (->Stock (new-account key secret)
+            (new-settings)
+            nonce-fn)))
 
-(def ^:dynamic *default-api* (new-api))
+(def ^:dynamic *default-stock* (new-stock))
 
 (defn- flatjoin [coll]
   (-> coll
@@ -107,13 +116,24 @@
                   headers
                   post-data)))
 
-(defn get-public [api trade-pair method]
-  (request-public (:settings api) trade-pair method))
+(defn get-public
+  "Get public info.
+  stock - create it with new-stock.
+  trade-pair - a currency trade pair (string symbol keyword).
+  method - one of :fee :depth :ticker :trades.
+  Returns a future with json response already converted to map with keywords as keys."
+  [stock trade-pair method]
+  (request-public (:settings stock) trade-pair method))
 
 (defmacro defnpub [rtype]
   `(defn ~(symbol (clojure.string/join `("get-" ~(name rtype))))
+     ~(str "Request " (name rtype) " with public API." \newline
+           "Argument trade-pair can be string, symbol or keyword (ex. :btc-usd)." \newline
+           "Dashes in keywords and symbold will be converted to underscores." \newline
+           "Returns a future with json response already converted to map with"
+           "keywords as keys.")
      ~'[trade-pair]
-     (get-public *default-api* ~'trade-pair ~rtype)))
+     (get-public *default-stock* ~'trade-pair ~rtype)))
 
 (defnpub :fee)
 (defnpub :trades)
@@ -121,15 +141,18 @@
 (defnpub :ticker)
 
 (defn trade
-  "api - api data,
+  "Calls trade API.
+  stock - create it with new-api,
   type - trade api method name (Can be string, keyword and symbol).
-  params - trade api method parameters.
-  Dashes will be converted to underscores."
-  [api type & params]
-  (request-trade (:account api)
-                 (:settings api)
+  params - trade api method parameters. Can be any type.
+  Symbols and keys will be converted to strings.
+  Dashes in symbols and keys will be converted to underscores.
+  Returns a future with json response already converted to map with keywords as keys."
+  [stock type & params]
+  (request-trade (:account stock)
+                 (:settings stock)
                  (assoc (when hash-map (apply hash-map (map clojure->btce params)))
-                        :nonce ((:nonce-fn api))
+                        :nonce ((:nonce-fn stock))
                         :method (name type))))
 
 
